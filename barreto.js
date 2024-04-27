f882e3d1bd4eda6b080573c9c8a2153be0a1e244
JulioiPolete <162917894+JulioiPolete@users.noreply.github.com>
2024-04-27 17:29:35 -0300

    Update barreto2.js

--- barreto.js
+++ barreto.js
@@ -13,7 +13,7 @@ button.addEventListener('click',function(){
      elemento.style.marginTop = "-150px";
      elemento.style.marginRight = "500px";
      elemento.style.radius = "20px";
-     elemento.style.backgroundImage = "url('https://media.discordapp.net/attachments/543913056057688066/1233842737053171762/fb86aa1e-b87b-407f-be57-ff4991bbe6ff.jpeg?ex=662e9118&is=662d3f98&hm=9e2d10b4f8f479439d6f4a7866f3c131f960430ef5138b18adf4d2d680d84322&=&format=webp&width=293&height=391')"
+     elemento.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/543913056057688066/1233842737053171762/fb86aa1e-b87b-407f-be57-ff4991bbe6ff.jpeg?ex=662e9118&is=662d3f98&hm=9e2d10b4f8f479439d6f4a7866f3c131f960430ef5138b18adf4d2d680d84322&')"
      elemento.style.backgroundPosition = "15% 30%";
    
 
